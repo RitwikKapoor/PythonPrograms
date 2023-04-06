fcf2d4e791ad2e1c8cb86649c41c90f44a41b48a
RitwikKapoor <kapoorritwik2020@gmail.com>
2023-04-06 22:51:46 +0530

    crud and json

--- ReadWriteJSON/readwritejson.py
+++ ReadWriteJSON/readwritejson.py
@@ -3,7 +3,7 @@ import json
 with open('info.json', 'r') as file:
     data = json.load(file)
 
-# data where age > 30 
+# data where age >= 30 
 filtered_data = [d for d in data if d['age'] >= 30]
 
 with open('filtered_info.json', 'w') as fi:
