52efa7746fa887cec80dc92962387328dce1f9ef
RitwikKapoor <kapoorritwik2020@gmail.com>
2023-04-06 22:49:39 +0530

    crud and json

---
+++ ReadWriteJSON/readwritejson.py
@@ -0,0 +1,10 @@
+import json
+
+with open('info.json', 'r') as file:
+    data = json.load(file)
+
+# data where age > 30 
+filtered_data = [d for d in data if d['age'] >= 30]
+
+with open('filtered_info.json', 'w') as fi:
+    json.dump(filtered_data, fi, indent=4)
